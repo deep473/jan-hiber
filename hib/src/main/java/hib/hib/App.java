3942ae704248750385d346720c70994af7914ad3
deep473 <deepztanshu@gmail.com>
2025-06-12 15:36:47 +0530

    june 12

--- hib/src/main/java/hib/hib/App.java
+++ hib/src/main/java/hib/hib/App.java
@@ -7,9 +7,16 @@ import org.hibernate.cfg.Configuration;
 
 public class App 
 {
-    public static void main( String[] args )
+	public static void main(String[] args) {
+		create();
+		read();
+//		update();
+//		delete();
+	}
+	
+    public static void create()
     {
-        Configuration cfg = new Configuration().configure();    
+    	Configuration cfg = new Configuration().configure();    
         SessionFactory fact = cfg.buildSessionFactory();      
         
         Session sess = fact.openSession();
@@ -21,4 +28,51 @@ public class App
         
         tx.commit();
     }
+    
+    public static void read()
+    {
+    	Configuration cfg = new Configuration().configure();    
+        SessionFactory fact = cfg.buildSessionFactory();      
+        
+        Session sess = fact.openSession();
+        
+        Student st = sess.find(Student.class, 2);
+        
+        System.out.println(st);
+    }
+    
+    public static void update()
+    {
+    	Configuration cfg = new Configuration().configure();    
+        SessionFactory fact = cfg.buildSessionFactory();      
+        
+        Session sess = fact.openSession();
+        
+        Student st = sess.find(Student.class, 3);
+        st.setName("deepika");
+        
+        Transaction tx = sess.beginTransaction();
+        sess.merge(st);
+        
+        tx.commit();
+    }
+    
+    public static void delete()
+    {
+        Configuration cfg = new Configuration().configure();    
+        SessionFactory fact = cfg.buildSessionFactory();      
+        
+        Session sess = fact.openSession();
+        
+        Student st = sess.find(Student.class, 3);
+        st.setName("deepika");
+        
+        Transaction tx = sess.beginTransaction();
+        sess.remove(st);
+        
+        tx.commit();
+    }
+    
+    
 }
+//https://github.com/deep473/jan-hiber
