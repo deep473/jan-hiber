16ab2e335898f95e25c1164e37e07d761ae74508
deep473 <deepztanshu@gmail.com>
2025-06-12 15:07:39 +0530

    h2

--- hib/src/main/java/hib/hib/App.java
+++ hib/src/main/java/hib/hib/App.java
@@ -1,16 +1,24 @@
 package hib.hib;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        Configuration cfg = new Configuration().configure();
+        Configuration cfg = new Configuration().configure();    
+        SessionFactory fact = cfg.buildSessionFactory();      
         
-        SessionFactory fact = cfg.buildSessionFactory();
+        Session sess = fact.openSession();
+        Transaction tx = sess.beginTransaction();
         
-        System.out.println(fact);
+        Student st = new Student(3, "deep", 7.4f, "cse");
+        
+        sess.persist(st);
+        
+        tx.commit();
     }
 }
